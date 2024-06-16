bc288bcff90ba904244f98439f68c40c0f7dbc5c
Tanuki <tanuki.evil@yandex.ru>
2024-06-16 15:54:10 +0300

    add templates and endpoints

--- page_analyzer/app.py
+++ page_analyzer/app.py
@@ -1,6 +1,8 @@
+from datetime import datetime
+
 import psycopg2
 import os
-from flask import Flask, render_template
+from flask import Flask, render_template, request, redirect, url_for
 from dotenv import load_dotenv
 
 app = Flask(__name__)
@@ -8,17 +10,35 @@ app = Flask(__name__)
 
 @app.route('/')
 def hello_world():
-    load_dotenv()
-    DATABASE_URL = os.getenv('DATABASE_URL')
-    conn = psycopg2.connect(DATABASE_URL)
     return render_template('index.html')
 
 
 @app.get('/urls')
 def get_urls():
-    pass
+    with psycopg2.connect("dbname=hexlet user=tanuki") as conn:
+        with conn.cursor() as cur:
+            cur.execute('SELECT * FROM urls ORDER BY id DESC;')
+            all_urls = cur.fetchall()
+    return render_template('urls.html', urls=all_urls)
 
 
-@app.get('/urls/<url_id>')
+@app.post('/urls')
+def post_urls():
+    url = request.form.get('url')
+    with psycopg2.connect("dbname=hexlet user=tanuki") as conn:
+        with conn.cursor() as cur:
+            cur.execute('INSERT INTO urls (name, created_at) VALUES (%s, %s);', (url, datetime.now()))
+            cur.execute('SELECT * FROM urls ORDER BY id DESC LIMIT 1;')
+            url_id = cur.fetchone()[0]
+    return redirect(url_for('get_url', url_id=url_id), 302)
+
+
+
+@app.get('/urls/<int:url_id>')
 def get_url(url_id):
-    pass
+    with psycopg2.connect("dbname=hexlet user=tanuki") as conn:
+        with conn.cursor() as cur:
+            cur.execute('SELECT * FROM urls WHERE id = %s;', (url_id, ))
+            url = cur.fetchone()
+    url_id, name, created_at = url
+    return render_template('url.html', id=url_id, name=name, created_at=created_at)
