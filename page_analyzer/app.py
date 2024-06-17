a7a954962fb1e14bc749bcbaf0c2ec5f8f4f51bd
tanuki <tanuki.evil@yandex.ru>
2024-06-17 22:51:49 +0300

    add validators and flash messages

--- page_analyzer/app.py
+++ page_analyzer/app.py
@@ -1,17 +1,21 @@
 import psycopg2
+from psycopg2 import extras
 import os
+import validators
 from datetime import datetime
-from flask import Flask, render_template, request, redirect, url_for
+from flask import Flask, render_template, request, redirect, url_for, flash, get_flashed_messages
 from dotenv import load_dotenv
 
-app = Flask(__name__)
 load_dotenv()
+app = Flask(__name__)
+app.config['SECRET_KEY'] = os.getenv('SECRET_KEY')
 DATABASE_URL = os.getenv('DATABASE_URL')
 
 
 @app.route('/')
 def hello_world():
-    return render_template('index.html')
+    messages = get_flashed_messages(with_categories=True)
+    return render_template('index.html', messages=messages)
 
 
 @app.get('/urls')
@@ -26,19 +30,24 @@ def get_urls():
 @app.post('/urls')
 def post_urls():
     url = request.form.get('url')
-    with psycopg2.connect(DATABASE_URL) as conn:
-        with conn.cursor() as cur:
-            cur.execute('INSERT INTO urls (name, created_at) VALUES (%s, %s);', (url, datetime.now()))
-            cur.execute('SELECT * FROM urls ORDER BY id DESC LIMIT 1;')
-            url_id = cur.fetchone()[0]
-    return redirect(url_for('get_url', url_id=url_id), 302)
+    if validators.url(url):
+        with psycopg2.connect(DATABASE_URL) as conn:
+            with conn.cursor() as cur:
+                cur.execute('INSERT INTO urls (name, created_at) VALUES (%s, %s);', (url, datetime.now()))
+                cur.execute('SELECT * FROM urls ORDER BY id DESC LIMIT 1;')
+                url_id = cur.fetchone()[0]
+        flash('Страница успешно добавлена', 'success')
+        return redirect(url_for('get_url', url_id=url_id), 302)
+    else:
+        flash('Некорректный URL', 'danger')
+        return redirect(url_for('hello_world'), 302)
 
 
 @app.get('/urls/<int:url_id>')
 def get_url(url_id):
     with psycopg2.connect(DATABASE_URL) as conn:
-        with conn.cursor() as cur:
+        with conn.cursor(cursor_factory=extras.DictCursor) as cur:
             cur.execute('SELECT * FROM urls WHERE id = %s;', (url_id,))
             url = cur.fetchone()
-    url_id, name, created_at = url
-    return render_template('url.html', id=url_id, name=name, created_at=created_at)
+    messages = get_flashed_messages(with_categories=True)
+    return render_template('url.html', url=url, messages=messages)
