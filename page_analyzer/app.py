d485ddcef70ec771b52cfe5110fd16823394d359
Tanuki <tanuki.evil@yandex.ru>
2024-06-18 15:13:28 +0300

    add new endpoint and update templates

--- page_analyzer/app.py
+++ page_analyzer/app.py
@@ -1,4 +1,5 @@
 import psycopg2
+import requests
 from psycopg2 import extras
 import os
 import validators
@@ -6,6 +7,7 @@ from datetime import datetime
 from flask import Flask, render_template, request, redirect, url_for, flash, get_flashed_messages
 from dotenv import load_dotenv
 from urllib.parse import urlparse
+from bs4 import BeautifulSoup
 
 load_dotenv()
 app = Flask(__name__)
@@ -21,8 +23,8 @@ def hello_world():
 @app.get('/urls')
 def get_urls():
     with psycopg2.connect(DATABASE_URL) as conn:
-        with conn.cursor() as cur:
-            cur.execute('SELECT * FROM urls ORDER BY id DESC;')
+        with conn.cursor(cursor_factory=extras.DictCursor) as cur:
+            cur.execute('SELECT urls.id, name, url_checks.created_at, url_checks.status_code FROM urls INNER JOIN url_checks ON urls.id = url_checks.url_id GROUP BY urls.id, name;')
             all_urls = cur.fetchall()
     return render_template('urls.html', urls=all_urls)
 
@@ -52,5 +54,20 @@ def get_url(url_id):
         with conn.cursor(cursor_factory=extras.DictCursor) as cur:
             cur.execute('SELECT * FROM urls WHERE id = %s;', (url_id,))
             url = cur.fetchone()
-    messages = get_flashed_messages(with_categories=True)
-    return render_template('url.html', url=url, messages=messages)
+            cur.execute('SELECT * FROM url_checks WHERE url_id = %s ORDER BY id DESC;', (url_id,))
+            checks = cur.fetchall()
+    messages = get_flashed_messages(with_categories=True)  # Сделать ли словарем
+    return render_template('url.html', url=url, checks=checks, messages=messages)
+
+
+@app.post('/urls/<url_id>/checks')
+def post_url(url_id):
+    with psycopg2.connect(DATABASE_URL) as conn:
+        with conn.cursor() as cur:
+            cur.execute('SELECT * FROM urls WHERE id = %s;', (url_id,))
+            url = cur.fetchone()[1]
+            response = requests.get(url, allow_redirects=False)
+            status_code = response.status_code
+            cur.execute('INSERT INTO url_checks (url_id, status_code, created_at) VALUES (%s, %s, %s);',
+                        (url_id, status_code, datetime.now()))
+    return redirect(url_for('get_url', url_id=url_id), 302)
