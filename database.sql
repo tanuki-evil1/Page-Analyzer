ccb04cd7d90356f2848a989922c882c62dae5f77
Tanuki <tanuki.evil@yandex.ru>
2024-06-18 15:12:42 +0300

    add new table

--- database.sql
+++ database.sql
@@ -1,5 +1,15 @@
 CREATE TABLE urls (
     id BIGINT PRIMARY KEY GENERATED ALWAYS AS IDENTITY,
-    name VARCHAR(255) UNIQUE,
-    created_at DATE
+    name VARCHAR(255) UNIQUE NOT NULL,
+    created_at DATE NOT NULL
+);
+
+CREATE TABLE url_checks (
+    id BIGINT PRIMARY KEY GENERATED ALWAYS AS IDENTITY,
+    url_id BIGINT REFERENCES urls (id) NOT NULL,
+    status_code INT,
+    h1 VARCHAR(255),
+    title VARCHAR(255),
+    description VARCHAR(255),
+    created_at DATE NOT NULL
 );
