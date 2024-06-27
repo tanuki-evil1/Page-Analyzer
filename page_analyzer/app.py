2ba39c51c81926b13cc6bf1427ca3e68dcca835d
Tanuki <tanuki.evil@yandex.ru>
2024-06-27 13:47:15 +0300

    add var in config

--- page_analyzer/app.py
+++ page_analyzer/app.py
@@ -16,6 +16,7 @@ load_dotenv()
 app = Flask(__name__)
 app.config['SECRET_KEY'] = os.getenv('SECRET_KEY')
 DATABASE_URL = os.getenv('DATABASE_URL')
+app.config['db_url'] = DATABASE_URL
 
 
 @app.route('/')
